a50271979f2ec26f052f69de771b4bdd4b5bb739
tresors-d <tresdongmo@gmail.com>
2023-06-28 17:32:44 +0100

    init

--- scripts/init_db.sql
+++ scripts/init_db.sql
@@ -5,113 +5,111 @@ USE  LABORATOIRE;
 Create table TEST_RESULT(
 	ID_TEST_RESULT integer not null,
     ID_TEST_REQUEST Varchar not null,
-    NAME String not null,
-    VALUE String                                                 
-);
+    NAME_TRES Varchar not null,
+    VALUE_TR Varchar );                                               
+
     
 
   Create table TEST_REQUEST(
-    ID_TEST_REQUEST                    Varchar                                         not null,
-    ID_PARTIENT                        Varchar                                         not null,
-    ID_TEST                           Varchar                                         not null,
-    ID_BILL                           Varchar                                         not null,
-    NAME                              String                                          not null,
-    DATE                              Date                                                    ,
-    STATUT                           String                                                 );
+    ID_TEST_REQUEST Varchar not null,
+    ID_PARTIENT Varchar not null,
+    ID_TEST Varchar not null,
+    ID_BILL Varchar not null,
+    NAME_TREQ Varchar not null,
+    DATE_TREQ Date,
+    STATUT_TREQ Varchar);
 
     
   Create table PATIENT(
-   ID_PATIENT                        Varchar                                       primary key,
-   NAME                              String                                           not null,       
-   FiRSTNAME                        String                                                  
-    ADRESS                            Varchar                                                 ,
-    BOTH_DATE                         Date                                                    ,
-    PHONE_NIMBER                        Int                                                    ,
-    SEX                               String                                                );
+   ID_PATIENT Varchar primary key,
+   NAME_P Varchar not null,       
+   FiRS_TNAME_P Varchar,                                                  
+    ADRESS_P Varchar,
+    BOTH_DATE_P Date,
+    PHONE_NIMBER Int,                                                    
+    SEX_P Varchar);
 
-  Create table TEST_REQUEST_PATIENT(
-   ID_PATIENT                       Varchar                                          not null ,
-   ID_TEST_REQUEST                   Varchar                                          not null);
+  
 
   create table BILL(
-   ID_BILL                          Varchar                                         primary key,
+   ID_BILL Varchar primary key,
                                                               
-   CREATION_DATE                     Date                                                    ,
-   TOTAL_AMOUNT                      Int                                                     ,
-   TOTAL_AMOUNT_PAID                  Int                                                    );
+   CREATION_DATE Date,
+   TOTAL_AMOUNT Int,
+   TOTAL_AMOUNT_PAID Int);
 
  Create table TEST(
-  ID_TEST                         Varchar                                         not null,
+  ID_TEST Varchar not null,
                                                            
-  ID_TEST_TYPE                     Varchar                                         not null,
-  NAME                            String                                          not null,
-  DESCRIPTION                    Vtring                                                 ,
-  PRICE                           Varchar                                                     ,
-  MIN_TIME_FRAME                    Varchar                                                 ,
-  MAX_TIME_FRAME                     Varchar                                                );
+  ID_TEST_TYPE Varchar not null,
+  NAME_T Varchar not null,
+  DESCRIPTION_T Varchar,
+  PRICE_T  integer,
+  MIN_TIME_FRAME Varchar,
+  MAX_TIME_FRAME Varchar);
  
  Create table TEST_TYPE(
 
-  ID_TEST_TYPE                    Varchar                                      primary key ,
-  NAME                           Varchar                                                 );
+  ID_TEST_TYPE Varchar primary key ,
+  NAME_TP Varchar);
 
  Create table RESULT(
-  ID_RESULT                    Varchar                                        not null  ,
-  ID_TEST                      Varchar                                        not null  ,
-  NAME                          String                                                   ,
-  DESCRIPTION                   String                                                  );
+  ID_RESULT Varchar  not null  ,
+  ID_TEST Varchar not null,
+  NAME_R Varchar,
+  DESCRIPTION_R Varchar);
 
  Create table RESULT_ITEM(
-  ID_RESULT_ITEM              Varchar                                        not null ,
-  ID_RESULT                   Varchar                                        not null ,
-  UNIT                         Varchar                                                  ,
-  TYPE                        String                                                  );                                                
+  ID_RESULT_ITEM Varchar not null ,
+  ID_RESULT Varchar not null ,
+  UNIT Varchar,
+  TYPE_RI Varchar);                                                
                                                                               
 
  Create table BILL_ITEM(
-  ID_BILL_ITEM                Varchar                                          not null  ,         
-  ID_BILL                     Varchar                                          not null  ,
-  PRICE                      Varchar                                                    ,
-  QUANTITY                   Int                                                        ,
-  DISCOUNT                    Varchar                                                    ,
-  DESCRIPTION                 Sting                                                    );
+  ID_BILL_ITEM Varchar not null,         
+  ID_BILL Varchar not null,
+  PRICE integer,
+  QUANTITY Int,
+  DISCOUNT integer,
+  DESCRIPTION Varchar);
 
  Create table ROLE(
-  ID_ROLE                     Varchar                                    primary key    ,
-  NAME                        String                                                    ,
-  DESCRIPTION                 String                                                    );
+  ID_ROLE Varchar primary key,    
+  NAME_R Varchar,
+  DESCRIPTION_RO Varchar);
 
  Create table USER_ROLE(
-  ID_ROLE                    Varchar                                      not null      ,
-  ID_USER                    Varchar                                      not null      );
+  ID_ROLE Varchar not null,
+  ID_USER Varchar not null);
 
  Create table USER(
-  ID_USER                   Varchar                                   primary key       ,
-  NAME                      String                                      not null        ,
- FIRSTNAME                   String                                                      ,
-  ADRESS                     Varchar                                                     ,
-  PHONE_NUNBER                 Int                                                        ,
-  PASSWORD                  Varchar                                     not null       );
+  ID_USER Varchar primary key,
+  NAME_T Varchar not null,
+ FIRST_NAME Varchar,
+  ADRESS Varchar,
+  PHONE_NUNBER integer,
+  PASSWORD_U Varchar not null);
 
  Create table PAYMENT(
-  ID_PAYMENT                Varchar                                      not null       ,
-  ID_USER                    Varchar                                      not null       ,
-  ID_BILL                    Varchar                                      not null       ,
-  DATE                        Date                                                       ,
-  AMOUNT                      Varchar                                                   );
+  ID_PAYMENT Varchar not null,
+  ID_USER Varchar not null,
+  ID_BILL Varchar not null,
+  DATE_PY Date,
+  AMOUNT_PY integer);
 
  Create table SAMPLE(
-  ID_SAMPLE                   Varchar                                     not null       ,
-  ID_SAMPLE_TYPE               Varchar                                     not null       ,
-  ID_TEST_REQUEST              Varchar                                     not null    ,
-  NAME                      string                                      not null       ,
-  PRODUCTION_DATE               Date                                                      ,
-  EXPIRATION_DATE               Date                                                      ,
+  ID_SAMPLE Varchar not null,
+  ID_SAMPLE_TYPE Varchar not null,
+  ID_TEST_REQUEST Varchar not nulL,
+  NAME_S Varchar not null,
+  PRODUCTION_DATE Date,
+  EXPIRATION_DATE Date);
 
  Create table SAMPLE_TYPE(
-  ID_SAMPLE_TYPE              Varchar                                  primary key       ,
-  NAME                        String                                                     ,
-  DESCRIPTION                String                                                     );
+  ID_SAMPLE_TYPE Varchar primary key,
+  NAME_ST Varchar,
+  DESCRIPTION Varchar);
 
  
  ALTER TABLE USER_ROLE ADD CONSTRAINT PK_USER_ROLE PRIMARY KEY (ID_ROLE, ID_USER);
